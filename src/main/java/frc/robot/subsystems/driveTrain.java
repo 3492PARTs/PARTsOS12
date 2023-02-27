02184ebdd95c498dc574a60000f408aaf4565248
Cameron <ckiw23@gmail.com>
2023-02-27 16:35:00 -0500

    added open loop ramp rate to drive train motors.

--- src/main/java/frc/robot/subsystems/driveTrain.java
+++ src/main/java/frc/robot/subsystems/driveTrain.java
@@ -62,11 +62,14 @@ public class driveTrain extends beanieDriveTrain {
     super(new AHRS(), new MotorControllerGroup(left2, left3), new MotorControllerGroup(right1, right3));
     Shuffleboard.getTab("primary").add(m_field);
 
+    left1.setOpenLoopRampRate(3);
     left2.setOpenLoopRampRate(3);
     left3.setOpenLoopRampRate(3);
 
     right1.setOpenLoopRampRate(3);
+    right2.setOpenLoopRampRate(3);
     right3.setOpenLoopRampRate(3);
+
 
   }
 
